fff118a1d31082d0c2a2924492a6314ec6aedfdc
Nikita <np.shche@gmail.com>
2023-11-22 14:46:09 +0300

    refactor cube spawner

--- Assets/Scripts/CubeSpawner.cs
+++ Assets/Scripts/CubeSpawner.cs
@@ -5,7 +5,6 @@ using Random = UnityEngine.Random;
 public class CubeSpawner : MonoBehaviour
 {
     [SerializeField] private Transform _prefab;
-    [SerializeField] private float _spawnBreakTime;
     [SerializeField] private Vector3 _startSpawnPoint;
     [SerializeField] private Vector3 _endSpawnPoint;
 
@@ -13,13 +12,10 @@ public class CubeSpawner : MonoBehaviour
 
     private void Update()
     {
-        // _lastSpawnTime += Time.deltaTime;
-        //&& _lastSpawnTime >= _spawnBreakTime
         if (Input.GetKey(KeyCode.Space))
         {
             var cube = Instantiate(_prefab, transform);
             cube.position = Vector3.Lerp(_startSpawnPoint, _endSpawnPoint, Random.Range(0f, 1f));
-            //_lastSpawnTime = 0;
         }
     }
 }
