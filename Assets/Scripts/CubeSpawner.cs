ea8e2931f915bcb4dd644916c24c7918998d5169
Nikita <np.shche@gmail.com>
2023-11-22 14:45:01 +0300

    refactor cube spawner & change camera position

--- Assets/Scripts/CubeSpawner.cs
+++ Assets/Scripts/CubeSpawner.cs
@@ -1,14 +1,25 @@
+using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class CubeSpawner : MonoBehaviour
 {
     [SerializeField] private Transform _prefab;
+    [SerializeField] private float _spawnBreakTime;
+    [SerializeField] private Vector3 _startSpawnPoint;
+    [SerializeField] private Vector3 _endSpawnPoint;
+
+    private float _lastSpawnTime;
 
     private void Update()
     {
+        // _lastSpawnTime += Time.deltaTime;
+        //&& _lastSpawnTime >= _spawnBreakTime
         if (Input.GetKey(KeyCode.Space))
         {
-            Instantiate(_prefab);
+            var cube = Instantiate(_prefab, transform);
+            cube.position = Vector3.Lerp(_startSpawnPoint, _endSpawnPoint, Random.Range(0f, 1f));
+            //_lastSpawnTime = 0;
         }
     }
 }
